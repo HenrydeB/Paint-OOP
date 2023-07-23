95105e050aebcbb28d3d64e38f6f4eee53f2ec21
Henry deBuchananne <69326962+HenrydeB@users.noreply.github.com>
2023-07-23 17:23:27 -0500

    update mouse action strat

--- src/model/MouseActionStrategy.java
+++ src/model/MouseActionStrategy.java
@@ -14,18 +14,15 @@ public class MouseActionStrategy {
     }
 
     public void strategize() {
-        //MouseActions actions = new MouseActions(state);
         IMouseAction action = null;
         switch(state.getActiveMouseMode()){
             case DRAW -> {
                 action = new DrawAction(state, start, end, false, false);
             }
             case MOVE -> {
-                //actions.moveShapes(start, end);
                 action = new MoveAction(false, false, start, end);
             }
             case SELECT -> {
-               // actions.selectShapes(start, end);
                 action = new SelectAction(start, end, state);
             }
         }
